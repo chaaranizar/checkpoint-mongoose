a53f672206e436a5e8b7ed8f001986e4a2bf11ae
chaaranizar <chaaranizar@gmail.com>
2023-01-17 09:08:50 +0100

    first commit

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './footer.css';
 const Footer = () => {
   return (
     <div>
-    <h3>MERN APP</h3>
-    <h3>Nizar</h3>
-    <h3> <img src='https://www.pngitem.com/pimgs/m/116-1167331_copyright-symbol-sign-black-white-circled-capital-all.png' alt='logo' />
+    <h3>MERN APP
+    Nizar
+     <img className='img' src='https://www.pngitem.com/pimgs/m/116-1167331_copyright-symbol-sign-black-white-circled-capital-all.png' alt='logo' />
     All Right Reserved
     </h3>
     </div>
